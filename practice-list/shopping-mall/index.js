d7a9cd52097ea4bf5e7895f607da69db762b3321
kongchip <myunghwan0321@gmail.com>
2023-03-10 20:44:17 +0900

    로그인 form UI 구현

--- practice-list/shopping-mall/index.js
+++ practice-list/shopping-mall/index.js
@@ -17,3 +17,13 @@ function navbarToggler() {
 //   .querySelectorAll('.navbar-toggler')[0]
 //   .addEventListener('click', navbarToggler);
 $('.navbar-toggler').on('click', navbarToggler);
+
+function openBtn() {
+  $('.black-bg').addClass('show-modal');
+}
+$('#login').on('click', openBtn);
+
+function closeBtn() {
+  $('.black-bg').removeClass('show-modal');
+}
+$('#close').on('click', closeBtn);
