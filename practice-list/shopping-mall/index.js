66a0e5bf55d7abc05f74e0536ac203d44e018ed3
kongchip <myunghwan0321@gmail.com>
2023-03-10 20:12:36 +0900

    navbar 기능 구현

---
+++ practice-list/shopping-mall/index.js
@@ -0,0 +1,19 @@
+function navbarToggler() {
+  // document.getElementsByClassName('list-group')[0].classList.toggle('show');
+  document.querySelector('.list-group').classList.toggle('show');
+  // document.querySelectorAll('.list-group')[0].classList.toggle('show');
+  $('.list-group').toggle('show');
+}
+
+// document
+// .getElementsByClassName('navbar-toggler')[0]
+// .addEventListener('click', navbarToggler);
+
+// document
+//   .querySelector('.navbar-toggler')
+//   .addEventListener('click', navbarToggler);
+
+// document
+//   .querySelectorAll('.navbar-toggler')[0]
+//   .addEventListener('click', navbarToggler);
+$('.navbar-toggler').on('click', navbarToggler);
