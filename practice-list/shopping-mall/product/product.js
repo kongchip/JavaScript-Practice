7e651632e2194b7064b9fb2f10d57165de849930
kongchip <myunghwan0321@gmail.com>
2023-03-17 01:57:22 +0900

    파일 위치 변경

---
+++ practice-list/shopping-mall/product/product.js
@@ -0,0 +1,28 @@
+//tab
+// for (let i = 0; i < document.querySelectorAll('.tab-button').length; i++) {
+//   let clickTab = document.querySelectorAll('.tab-button')[i];
+//   clickTab.addEventListener('click', function () {
+//     let tabOrange = document.querySelectorAll('.tab-button');
+//     let tabShow = document.querySelectorAll('.tab-content');
+
+//     tabOrange.forEach((el) => {
+//       el.classList.remove('orange');
+//       tabOrange[i].classList.add('orange');
+//     });
+//     tabShow.forEach((el) => {
+//       el.classList.remove('show');
+//       tabShow[i].classList.add('show');
+//     });
+//   });
+// }
+let tabButton = document.querySelectorAll('.tab-button');
+$('.tabList').click(function (e) {
+  tabHandler(e.target.dataset.id);
+});
+
+function tabHandler(num) {
+  $('.tab-button').removeClass('orange');
+  $('.tab-button').eq(num).addClass('orange');
+  $('.tab-content').removeClass('show');
+  $('.tab-content').eq(num).addClass('show');
+}
