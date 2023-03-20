60440782d31c44160463eb34a583ae6cfdb53cf0
kongchip <myunghwan0321@gmail.com>
2023-03-20 13:15:56 +0900

    detail 페이지 select item 코드 리펙토링

--- practice-list/shopping-mall/product/detail.js
+++ practice-list/shopping-mall/product/detail.js
@@ -1,3 +1,35 @@
+let car = {
+  name: '모자',
+  price: 50000,
+};
+
+let pantsSize = [28, 30, 32, 34];
+let shirtSize = [95, 100, 105];
+
+//select item
+$('.form-select')
+  .eq(0)
+  .on('input', function () {
+    let value = this.value;
+    if (value === '셔츠') {
+      $('.form-select').eq(1).removeClass('form-hide');
+      $('.form-select').eq(1).html('');
+
+      shirtSize.forEach(function (data) {
+        $('.form-select').eq(1).append(`<option>${data}</option`);
+      });
+    } else if (value === '바지') {
+      $('.form-select').eq(1).removeClass('form-hide');
+      $('.form-select').eq(1).html('');
+
+      pantsSize.forEach(function (data) {
+        $('.form-select').eq(1).append(`<option>${data}</option>`);
+      });
+    } else if (value === '모자') {
+      $('.form-select').eq(1).addClass('form-hide');
+    }
+  });
+
 //tab
 // for (let i = 0; i < document.querySelectorAll('.tab-button').length; i++) {
 //   let clickTab = document.querySelectorAll('.tab-button')[i];
@@ -26,10 +58,3 @@ function tabHandler(num) {
   $('.tab-content').removeClass('show');
   $('.tab-content').eq(num).addClass('show');
 }
-
-let car = {
-  name: '모자',
-  price: 5000,
-};
-document.querySelector('.car-title').innerHTML = car.name;
-document.querySelector('.car-price').innerHTML = car.price;
